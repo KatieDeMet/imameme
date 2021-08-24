cb0dafbf40ddca17ea289e1bd76909b004cb4bdd
Katie DeMet <katie.demet7@gmail.com>
2021-08-24 08:30:30 -0500

    server update

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 // const proxy = require('./controller.js')
 const cntl = require('./controller.js')
+const port = process.env.PORT || 7777
 
 const app = express()
 app.use(express.json())
@@ -9,8 +10,12 @@ app.use(cors())
 app.use(express.static('client'))
 // app.use('/', proxy())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
 app.get('/img', cntl.getImg)
 app.post('/img', cntl.addImg)
 
 
-app.listen(7777, () => console.log("Port 7777 is up!"))
+app.listen(port, () => console.log(`Port ${port} is up!`))
